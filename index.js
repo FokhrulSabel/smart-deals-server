91d3cc2da8b8727fe00a7de5fed1a09e2198d78d
FokhrulSabel <fokhrulislam316@gmail.com>
2026-01-11 23:44:57 +0600

    Recap CRUD

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection URI
-const uri = "mongodb+srv://simpleDBUser:<db_password>@firstdb.egqdlgn.mongodb.net/?appName=firstdb";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,16 +26,67 @@ app.get("/", (req, res) => {
   res.send("simple curd operation server is running");
 });
 
-
 async function run() {
-    try{
+  try {
+    await client.connect();
 
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }finally{
-        // await client.close();
-    }
+    const db = client.db("smartDealsDB");
+    const productsCollection = db.collection("products");
+
+    // Create Operation - Add a new product
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Read Operation - Get a product by ID
+    app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+      });
+
+    // Read Operation - Get all products
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // Update Operation - Update a product by ID
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedProduct = req.body;
+      const updateDoc = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+          // category: updatedProduct.category,
+        },
+      };
+
+      const result = await productsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // Delete Operation - Delete a product by ID
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
